f0a3c83ebf081174261dfe7ceff0375a4e050ec2
Suryansh Chauhan <suryanshchauhan1016star@gmail.com>
2024-06-13 23:13:52 +0530

    login sign up basic structure

--- app/browse/page.js
+++ app/browse/page.js
@@ -1,18 +1,20 @@
 "use client"; 
 import React , { useEffect, useState } from "react";
 import axios from "axios";
+import ImageCard from "../../components/ImageCard";
 
 
 
 export default function Browse() {
 
-const [data ,setData] = useState([]);
+const [cardData ,setCardData] = useState([]);
 
     useEffect(()=>{
         const getRecomendations = async () =>{
             try{
-                const response = await axios.get(`https://api.jikan.moe/v4/recommendations/anime`);
-                setData(response)
+                const response = await axios.get(`https://api.jikan.moe/v4/top/anime`);
+                await setCardData(response.data.data);
+                console.log(response.data.data)
             } catch(err){
                 console.log('error occured' , err)
             }
@@ -25,6 +27,10 @@ const [data ,setData] = useState([]);
     return(
         <>
         this is Browser
+        {cardData.map(item =>(
+            
+          <ImageCard key = {item.id} image = {item.images.jpg.large_image_url} />
+        ))}
         </>
     );
 }
--- app/signin/page.js
+++ app/signin/page.js
@@ -1,7 +1,22 @@
+import { Stack, TextField } from '@mui/material';
+import Paper from '@mui/material/Paper';
+
+
 export default function SignIn(){
 return(
     <>
-    this is signIn page
+    <Paper elevation={3} >
+        <h2 style={{marginBottom:'1rem'}}>
+            Sign In
+        </h2>
+        <Stack direction={'column'} sx={{width:'500px' , gap:'1rem'}}>
+<TextField id="outlined-basic" label="Username" variant="outlined" ></TextField>
+<TextField id="outlined-basic" label="Password" variant="outlined" type='password'></TextField>
+<h3>Don't have a account ? <span> <a href="/signup"> create account</a></span> </h3>
+        </Stack>
+
+    </Paper>
+    
     </>
 )
 }
